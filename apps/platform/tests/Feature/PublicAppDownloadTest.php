6888348255a21257e239ef065af76439018e9500
Saumya Vakharia <saumyavakharia@Saumyas-MacBook-Air.local>
2026-08-10 00:01:39 +0530

    Add public latest PUNIT ERP app download

---
+++ apps/platform/tests/Feature/PublicAppDownloadTest.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+
+class PublicAppDownloadTest extends TestCase
+{
+    public function test_public_home_and_login_show_latest_app_download(): void
+    {
+        $this->get('/')
+            ->assertOk()
+            ->assertSee('Download Latest PUNIT ERP App')
+            ->assertSee(route('app.download.latest'));
+
+        $this->get('/login')
+            ->assertOk()
+            ->assertSee('Download Latest PUNIT ERP App')
+            ->assertSee(route('app.download.latest'));
+    }
+
+    public function test_latest_download_route_points_to_production_signed_apk(): void
+    {
+        $this->get('/downloads/latest')
+            ->assertRedirect('/downloads/PUNIT-ERP-v1.1.16-build21.apk');
+    }
+}
